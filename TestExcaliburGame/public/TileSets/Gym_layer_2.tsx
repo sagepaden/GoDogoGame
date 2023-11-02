731a307d2b41743c3a59846e9caedff2050db2bc
sagepaden <sagepaden@gmail.com>
2023-11-02 11:31:37 -0700

    added collision to map, still clippinmg through some tiles

--- TestExcaliburGame/public/TileSets/Gym_layer_2.tsx
+++ TestExcaliburGame/public/TileSets/Gym_layer_2.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="Gym_layer_2" tilewidth="16" tileheight="16" tilecount="285" columns="19">
- <image source="/TileSets/Gym_layer_2.png" width="304" height="240"/>
+ <image source="Gym_layer_2.png" width="304" height="240"/>
 </tileset>
